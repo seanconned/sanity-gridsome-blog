d4867c8162cedcd4f5e5868b36cef9267eedab7f
seanconned <seanconned@gmail.com>
2022-02-04 08:31:32 -0600

    Update AuthorCard.vue

--- web/src/components/AuthorCard.vue
+++ web/src/components/AuthorCard.vue
@@ -14,8 +14,7 @@
     <p class="author__intro">{{$static.settings.description}}</p>
 
     <p class="author__links">
-      <a href="//twitter.com/sanity_io">Follow on Twitter</a>
-      <a href="//github.com/sanity-io/sanity-template-gridsome-blog">GitHub</a>
+      <a href="//linkedin.com/in/sean-conner/">Connect on LinkedIn</a>
     </p>
   </div>
 </template>
