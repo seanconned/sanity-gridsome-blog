c9b68e9008769bf632a6c02b3a8f8e972c12bd87
seanconned <seanconned@gmail.com>
2022-02-05 10:02:44 -0600

    Update gridsome.config.js

--- web/gridsome.config.js
+++ web/gridsome.config.js
@@ -13,9 +13,9 @@ const clientConfig = require('./client-config')
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  siteName: 'Gridsome Blog Starter',
+  siteName: 'Sean Conner',
   siteDescription:
-    'A simple, hackable & minimalistic starter for Gridsome that uses structured content from Sanity.io.',
+    'Sean Conner is content modeling, doodling, and board gaming in Manhappiness, Kansas.',
 
   templates: {
     SanityPost: '/:slug__current'
