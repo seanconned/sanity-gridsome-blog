83a6d5e44d3dc918d190a86f03c69d93b4d4f17a
seanconned <seanconned@gmail.com>
2022-08-05 15:58:45 -0500

    Create musing.js

---
+++ studio/schemas/documents/musing.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'musing',
+  type: 'document',
+  title: 'Musing',
+  fields: [
+    {
+      name: 'description',
+      type: 'text',
+      title: 'Description'
+    }
+     {
+      name: 'location',
+      type: 'text',
+      title: 'Where the musing came from'
+    }
+  ]
+}
