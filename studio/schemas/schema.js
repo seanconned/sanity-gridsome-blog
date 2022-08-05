b1a4348a922f257a71021dfc18f76112aae71028
seanconned <seanconned@gmail.com>
2022-08-05 16:04:59 -0500

    Update schema.js

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -8,6 +8,7 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 import author from './documents/author'
 import category from './documents/category'
 import post from './documents/post'
+import post from './documents/musings'
 import siteSettings from './documents/siteSettings'
 
 // Object types
@@ -30,6 +31,7 @@ export default createSchema({
     post,
     category,
     author,
+    musings,
     mainImage,
     authorReference,
     bodyPortableText,
